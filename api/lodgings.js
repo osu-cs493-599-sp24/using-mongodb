722e6630478c45e94eb3edb7c16c2374d6e730f9
Rob Hess <hessro@oregonstate.edu>
2024-05-01 11:36:35 -0700

    Add code from 5/1 lecture.

--- api/lodgings.js
+++ api/lodgings.js
@@ -1,17 +1,78 @@
 const { Router } = require('express')
+const { ObjectId } = require("mongodb")
+
+const { getDb } = require("../mongodb")
+
 const router = Router()
 
-router.get('/', function (req, res, next) {
-    res.status(200).send({})
+router.get('/', async function (req, res, next) {
+    const db = getDb()
+    try {
+        const collection = db.collection("lodgings")
+        const count = await collection.countDocuments()
+
+        const pageSize = 10
+        let page = parseInt(req.query.page) || 1
+        page = page < 1 ? 1 : page
+        const offset = (page - 1) * pageSize
+
+        const results = await collection.find({})
+            .sort({ _id: 1 })
+            .skip(offset)
+            .limit(pageSize)
+            .toArray()
+
+        res.status(200).send({
+            lodgings: results,
+            count: count
+        })
+    } catch (e) {
+        next(e)
+    }
 })
 
-router.post('/', function (req, res, next) {
-    res.status(201).send({})
+router.post('/', async function (req, res, next) {
+    const db = getDb()
+    try {
+        const collection = db.collection("lodgings")
+        const result = await collection.insertOne(req.body)
+        res.status(201).send({
+            id: result.insertedId
+        })
+    } catch (e) {
+        next(e)
+    }
 })
 
-router.get('/:id', function (req, res, next) {
+router.get('/:id', async function (req, res, next) {
     const id = req.params.id
-    res.status(200).send({})
+    const db = getDb()
+    try {
+        const collection = db.collection("lodgings")
+
+        // const results = await collection.find({
+        //     _id: new ObjectId(id)
+        // }).toArray()
+
+        const results = await collection.aggregate([
+            { $match: { _id: new ObjectId(id) } },
+            { $lookup: {
+                from: "reservations",
+                localField: "_id",
+                foreignField: "lodgingId",
+                as: "reservations"
+            }}
+        ]).toArray()
+
+
+        if (results.length > 0) {
+            res.status(200).send(results[0])
+        } else {
+            next()
+        }
+    } catch (e) {
+        next(e)
+    }
 })
 
 router.patch('/:id', function (req, res, next) {
--- api/reservations.js
+++ api/reservations.js
@@ -1,8 +1,24 @@
 const { Router } = require('express')
+const { ObjectId } = require("mongodb")
+
+const { getDb } = require("../mongodb")
+
 const router = Router()
 
-router.post('/', function (req, res, next) {
-    res.status(201).send({})
+router.post('/', async function (req, res, next) {
+    const db = getDb()
+    try {
+        const collection = db.collection("reservations")
+        const result = await collection.insertOne({
+            ...req.body,
+            lodgingId: new ObjectId(req.body.lodgingId)
+        })
+        res.status(201).send({
+            id: result.insertedId
+        })
+    } catch (e) {
+        next(e)
+    }
 })
 
 module.exports = router
